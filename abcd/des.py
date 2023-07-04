a1d517fbb2b2cb816f7ab8d7c42233db29c1ff19
raglajayakrishna <jayakrishnamarolix@gmail.com>
2023-07-04 15:22:14 +0530

    second

---
+++ abcd/des.py
@@ -0,0 +1,156 @@
+print("hello",end=' ')
+print("good morning",end=" ")
+print("krishna")
+
+a,b,c=10,20,30
+print("values is:",a,b,c,sep=",")
+
+s='abcdefghijk'
+print(s[8:-1:-2])
+
+print(s[4:-2:2])
+
+s='abcdefghijk'
+print(s[2:-4:5])
+
+print(s[4:-4:2])
+
+k='ragala jaya krishna'
+print(k[4:-2:3])
+
+print(k[5:-3:2])
+
+print(k[6:-4:2])
+
+
+name= input("enter the name")
+if name=="jaya krishna":
+    print("hellow how are you",name)
+else:
+    print("hellow you are not jaya krishna")
+
+
+
+    name = input("enter the name")
+    if name == "krishna":
+        print("hellow how are you ",name)
+    elif name == "raju":
+        print("hellow you are not krishna", name)
+    elif name == "sunny":
+        print("hellow how are you",name)
+    else:
+        print("you are not authorised person")
+
+
+a=33
+b=200
+if b>a:
+    print("b is greater than a")
+
+    
+
+a = 33
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+
+
+age = 18
+if ((age>= 8) and (age<= 12)):
+  print("you are allowed welcome !")
+else:
+  print("sorry ! you are not allowed. bye !") 
+
+
+  age = 18
+if ((age>= 8) and (age<= 12)):
+    print("you are allowed . welcome !")
+else:
+    print("sorry ! you are not allowed . bye!")
+
+
+
+
+    
+
+
+
+
+    for i in range(4):
+     for j in range(4):
+        print("i={} and j={}" . format(i,j))
+
+
+
+        name=""
+    while name!="raju":
+       name=input("enter user name")
+    print("hello" ,name,"thank you for authenticating")
+
+
+
+    name=""
+    password=""
+    while (name!="raju") and (password!="123"):
+       name=input("enter user name")
+       password=input("enter the password")
+    print("hello" ,name,"thank you for authenticating")
+
+
+
+
+    name=""
+    password=""
+    while (name!="raju") or (password!="123"):
+       name=input("enter user name")
+       password=input("enter the password")
+    print("hello" ,name,"thank you for authenticating")
+
+
+
+    name=""
+    password=""
+    while (name != "raju") and (password != "123"):
+       name=input("enter user name")
+       password=input("enter the password")
+    print("hello" ,name,"thank you for authenticating")
+
+
+
+rows = 5
+for i in range(1, rows +1):
+ for j in range(1, i + 1):
+       print(j, end=' ')
+       print('')
+
+
+
+rows = 5
+num = rows
+for i in range(rows, 0, -1):
+ for j in range(0, i):
+   print(num, end=' ')
+   print("\r")
+
+
+rows = 6
+for i in range(rows):
+   for j in range(i):
+    print(i, end=' ')
+    print('')
+   
+
+    
+     
+
+
+
+
+
+
+
+
+
+
